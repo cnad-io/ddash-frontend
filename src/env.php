3ecb2df6b624710f40d3347c905c05916bf13346
mcaballol <mcaballol@redhat.com>
2019-10-09 01:58:26 -0300

    fix variables

--- src/env.php
+++ src/env.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: text/javascript');
 ?>
-
 window.gameRoomUrl = "<?php echo getenv('GAME_ROOM_REMOTE_ADDR') ?>"
 window.waitingRoomUrl = "<?php echo getenv('WAITING_ROOM_REMOTE_ADDR') ?>"
